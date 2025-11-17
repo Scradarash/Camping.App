1ddcdc6e0660c9adc49337a524a15242b25f959a
Jacob Arie Romkes <s1209077@student.windesheim.nl>
2025-11-17 13:37:22 +0100

    Tweede vorm van de projectstructuur aangemaakt. -In de mappenstructuur placeholder bestanden teogevoegd aangezien Github geen lege folders ziet

---
+++ Camping.App/Views/Placeholder.xaml.cs
@@ -0,0 +1,9 @@
+namespace Camping.App.Views;
+
+public partial class Placeholder : ContentPage
+{
+	public Placeholder()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Camping.Core/Interfaces/Services/Placeholder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camping.Core.Interfaces.Services
+{
+    internal class Placeholder
+    {
+    }
+}
