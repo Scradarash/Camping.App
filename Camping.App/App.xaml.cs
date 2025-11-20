6e43dc98fe6629417f716511671c4a560de8735c
Bram <108123835+BramSpr@users.noreply.github.com>
2025-11-20 14:33:07 +0100

    Added background
    - Opnieuw gemaakt van de Mainpage
    - Afbeelding toegevoegd
    - Logica toegevoegd voor het tonen van de afbeelding op basis van de grootte van het beeldscherm
    - Logica toegevoegd voor het full-screen weergeven van de afbeelding

--- Camping.App/App.xaml.cs
+++ Camping.App/App.xaml.cs
@@ -9,7 +9,14 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            return new Window(new AppShell())
+            {
+                Width = DeviceDisplay.Current.MainDisplayInfo.Width,
+                Height = DeviceDisplay.Current.MainDisplayInfo.Width,
+                X = 100,
+                Y = 100
+
+            };
         }
     }
 }
--- Camping.App/MauiProgram.cs
+++ Camping.App/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace Camping.App
 {
@@ -14,9 +15,33 @@ namespace Camping.App
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                // Make sure to add "using Microsoft.Maui.LifecycleEvents;" in the top of the file 
+                events.AddWindows(windowsLifecycleBuilder =>
+                {
+                    windowsLifecycleBuilder.OnWindowCreated(window =>
+                    {
+                        window.ExtendsContentIntoTitleBar = false;
+                        var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+                        switch (appWindow.Presenter)
+                        {
+                            case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                                overlappedPresenter.SetBorderAndTitleBar(false, false);
+                                overlappedPresenter.Maximize();
+                                break;
+                        }
+                    });
+                });
+            });
+#endif
+
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
