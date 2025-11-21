1810e520f426cf08230d2b8d866d412292b9d44c
Bram <108123835+BramSpr@users.noreply.github.com>
2025-11-21 15:45:13 +0100

    More buttons
    Meer staanplaatsen toegevoegd
    Commit om te testen of de plaatsing van deze knop het zelfde is op een andere computer

--- Camping.App/Views/PlattegrondView.xaml.cs
+++ Camping.App/Views/PlattegrondView.xaml.cs
@@ -4,7 +4,10 @@ namespace Camping.App.Views
 {
     public partial class PlattegrondView : ContentPage
     {
-        int clicks = 1;
+        int clicks1 = 1;
+        int clicks2 = 1;
+        int clicks3 = 1;
+
         public PlattegrondView(PlattegrondViewModel viewModel)
         {
             InitializeComponent();
@@ -13,11 +16,24 @@ namespace Camping.App.Views
 
         private void Staanplaats1_Clicked(object sender, EventArgs e)
         {
-            clicks++;
-            Staanplaats1.Text = $"Staanplaats {clicks}";
+            clicks1++;
+            Staanplaats1.Text = $"Staanplaats {clicks1}";
             SemanticScreenReader.Announce(Staanplaats1.Text);
         }
 
+        private void Staanplaats2_Clicked(object sender, EventArgs e)
+        {
+            clicks2++;
+            Staanplaats2.Text = $"Staanplaats {clicks2}";
+            SemanticScreenReader.Announce(Staanplaats2.Text);
+        }
+
+        private void Staanplaats3_Clicked(object sender, EventArgs e)
+        {
+            clicks3++;
+            Staanplaats3.Text = $"Staanplaats {clicks3}";
+            SemanticScreenReader.Announce(Staanplaats3.Text);
+        }
         private void OnCancelButtonPointerEntered(object sender, PointerEventArgs e)
         {
              Staanplaats1.BackgroundColor = Colors.Green;
@@ -26,6 +42,25 @@ namespace Camping.App.Views
         private void OnCancelButtonPointerExited(object sender, PointerEventArgs e)
         {
             Staanplaats1.BackgroundColor = Colors.DarkGreen; // Revert to original color
+        }
+
+        private void OnCancelButtonPointerEnteredST2(object sender, PointerEventArgs e)
+        {
+            Staanplaats2.BackgroundColor = Colors.Green;
+        }
+
+        private void OnCancelButtonPointerExitedST2(object sender, PointerEventArgs e)
+        {
+            Staanplaats2.BackgroundColor = Colors.DarkGreen; // Revert to original color
+        }
+        private void OnCancelButtonPointerEnteredST3(object sender, PointerEventArgs e)
+        {
+            Staanplaats3.BackgroundColor = Colors.Green;
+        }
+
+        private void OnCancelButtonPointerExitedST3(object sender, PointerEventArgs e)
+        {
+            Staanplaats3.BackgroundColor = Colors.DarkGreen; // Revert to original color
         }
     }
 }
