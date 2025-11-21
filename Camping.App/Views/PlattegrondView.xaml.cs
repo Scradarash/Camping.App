fab882881803ac30fea2f85b9f39a88a2f002211
Bram <108123835+BramSpr@users.noreply.github.com>
2025-11-21 14:55:39 +0100

    Added button
    - Knop gemaakt
    - Knop gestyled
    - Action aan knop gekoppeld
    - Hover aan knop toegevoegd (wat een gezeur)
    - Action laten doorspelen in UI

--- Camping.App/Views/PlattegrondView.xaml.cs
+++ Camping.App/Views/PlattegrondView.xaml.cs
@@ -4,10 +4,28 @@ namespace Camping.App.Views
 {
     public partial class PlattegrondView : ContentPage
     {
+        int clicks = 1;
         public PlattegrondView(PlattegrondViewModel viewModel)
         {
             InitializeComponent();
             BindingContext = viewModel;
+        }
+
+        private void Staanplaats1_Clicked(object sender, EventArgs e)
+        {
+            clicks++;
+            Staanplaats1.Text = $"Staanplaats {clicks}";
+            SemanticScreenReader.Announce(Staanplaats1.Text);
+        }
+
+        private void OnCancelButtonPointerEntered(object sender, PointerEventArgs e)
+        {
+             Staanplaats1.BackgroundColor = Colors.Green;
+        }
+
+        private void OnCancelButtonPointerExited(object sender, PointerEventArgs e)
+        {
+            Staanplaats1.BackgroundColor = Colors.DarkGreen; // Revert to original color
         }
     }
 }
