7227cf11cc7a65f7f09c975c0d6aac84874bf619
Milan <mp09092003@gmail.com>
2025-12-04 20:02:39 +0100

    model gemaakt voor gast

---
+++ Camping.Core/Models/Gast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camping.Core.Models
+{
+    public class Gast
+    {
+        public required int Id {get; set;}
+        public required string Naam { get; set; }
+        public required DateOnly Geboortedatum { get; set; }
+        public required string Email { get; set; }
+        public required string Telefoon { get; set; }
+
+    }
+}
