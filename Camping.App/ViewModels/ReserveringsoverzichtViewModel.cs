2308569c2afd3b4cc8a95cc4966bf142a647baeb
Milan <s1211510@student.windesheim.nl>
2025-12-16 01:46:42 +0100

    toevoeging van async & await bij MaakReservering

--- Camping.App/ViewModels/ReserveringsoverzichtViewModel.cs
+++ Camping.App/ViewModels/ReserveringsoverzichtViewModel.cs
@@ -174,7 +174,7 @@ public partial class ReserveringsoverzichtViewModel : ObservableObject
             _reservatieDataService.Telefoonnummer = Telefoonnummer;
             _reservatieDataService.SelectedAccommodatie = SelectedAccommodatie;
 
-            _reserveringService.MaakReservering(
+            await _reserveringService.MaakReserveringAsync(
                 _reservatieDataService.StartDate.Value,
                 _reservatieDataService.EndDate.Value,
                 _reservatieDataService.SelectedVeld,
