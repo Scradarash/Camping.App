167e897f3a0869257fc6703c42ba47f5b585745b
Scradarash <kaj.treep.1997@gmail.com>
2025-12-17 16:20:21 +0100

    Faciliteiten toegevoegd aan plattegrond
    Heb de faciliteiten werkend op de kaart gekregen, database geupdated en hover animaties toegevoegd.
    
    - Plattegrond:
       - Interactieve faciliteit-icoontjes toegevoegd aan de kaart
       - FaciliteitButtonStyle aangemaakt in XAML voor hover-effecten en om de standaard grijze achtergrond te voorkomen
       - CreateImageButton geïmplementeerd met async click afhandeling zodat de knop vergroot blijft zolang de popup open is
       - PlattegrondView.xaml.cs uitgebreid met AddFaciliteitButtons logica
    
    - ViewModel & Services
       - IFaciliteitService geïnjecteerd in PlattegrondViewModel om data op te halen
       - ShowFaciliteitInfo command toegevoegd om details te tonen in een popup
       - LoadAreas uitgebreid om de nieuwe Faciliteiten lijst te vullen
    
    - Algemeen
       - Variabelenamen in PlattegrondView duidelijker gemaakt (zoals xposition, yposition)
       - Database uitgebreid en in de commit toegevoegd

--- Camping.App/Resources/Database/CampingAppDump_17-12-2025.sql
+++ Camping.App/Resources/Database/CampingAppDump_17-12-2025.sql
@@ -27,6 +27,7 @@ DROP TABLE IF EXISTS `accommodatie_types`;
 CREATE TABLE `accommodatie_types` (
   `id` int NOT NULL AUTO_INCREMENT,
   `naam` varchar(50) NOT NULL,
+  `prijs` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -37,8 +38,38 @@ CREATE TABLE `accommodatie_types` (
 
 LOCK TABLES `accommodatie_types` WRITE;
 /*!40000 ALTER TABLE `accommodatie_types` DISABLE KEYS */;
-INSERT INTO `accommodatie_types` VALUES (1,'Tent'),(2,'Caravan'),(3,'Camper'),(4,'Chalet');
+INSERT INTO `accommodatie_types` VALUES (1,'Tent',2.99),(2,'Caravan',6.99),(3,'Camper',7.99),(4,'Chalet',49.99);
 /*!40000 ALTER TABLE `accommodatie_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `faciliteiten`
+--
+
+DROP TABLE IF EXISTS `faciliteiten`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `faciliteiten` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `naam` varchar(100) NOT NULL,
+  `omschrijving` text,
+  `image_name` varchar(100) NOT NULL,
+  `x_position` float NOT NULL DEFAULT '0',
+  `y_position` float NOT NULL DEFAULT '0',
+  `width` float NOT NULL DEFAULT '0.05',
+  `height` float NOT NULL DEFAULT '0.07',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faciliteiten`
+--
+
+LOCK TABLES `faciliteiten` WRITE;
+/*!40000 ALTER TABLE `faciliteiten` DISABLE KEYS */;
+INSERT INTO `faciliteiten` VALUES (1,'Wasserette','Draai hier uw wasjes. Muntjes verkrijgbaar bij de receptie!','wasserette.png',0.3521,0.305,0.02656,0.04444),(2,'Toiletten West','Toiletten in sanitairgebouw west, dagelijks gereinigd!','toilet.png',0.3521,0.355,0.02656,0.04444),(3,'Douches','Warme douches. Muntjes verkrijgbaar bij de receptie!','douches.png',0.3521,0.405,0.02656,0.04444),(4,'Zwembad','Verwarmd buitenzwembad. Geopend 09:00 - 20:00.','zwembad.png',0.421,0.2645,0.02656,0.04444),(5,'Restaurant','De Gulle Bever - Snacks en plateservice.','restaurant.png',0.5272,0.3499,0.02656,0.04444),(6,'Speeltuin','Klimrek en schommels voor de kleintjes!','speeltuin.png',0.5085,0.2315,0.02656,0.04444),(7,'Zwemmeer & Strand','Heerlijk recreatiemeer met zandstrand. Het is ook mogelijk om te barbequeen op het strand!','strand.png',0.6615,0.22,0.02656,0.04444),(8,'Parkeerplaats','Centrale parkeerplaats. Gelieve auto\'s alleen gebruiken om caravans op hun plek te zetten!','parkeerplek.png',0.651,0.466,0.02656,0.04444),(9,'Receptie','Meld u hier aan. Wij helpen u graag!','receptie.png',0.452,0.466,0.02656,0.04444),(10,'Afwasruimte','Publieke afwasruimte waar u al uw borden, bestek en andere vaat kan wassen!','afwasruimte.png',0.587,0.3527,0.02656,0.04444),(11,'Afvalbak Groepsveld','Houd de camping schoon!','afvalbak.png',0.069,0.35,0.02656,0.04444),(12,'Afvalbak Noord','Houd de camping schoon!','afvalbak.png',0.497,0.105,0.02656,0.04444),(13,'Afvalbak Zuid','Houd de camping schoon!','afvalbak.png',0.061,0.875,0.02656,0.04444),(14,'Afvalbak Staatseveld','Houd de camping schoon!','afvalbak.png',0.5665,0.515,0.02656,0.04444),(15,'Verzamelpunt','Verzamelplaats voor gezamenlijke activiteiten en in het geval van calamiteiten.','verzamelpunt.png',0.61,0.8,0.02656,0.04444),(16,'Toiletten Oost','Toiletten in sanitairgebouw oost, dagelijks gereinigd!','toilet.png',0.587,0.4027,0.02656,0.04444),(17,'Afvalbak Centraal','Houd de camping schoon.','afvalbak.png',0.3519,0.512,0.02656,0.04444);
+/*!40000 ALTER TABLE `faciliteiten` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -55,7 +86,7 @@ CREATE TABLE `gasten` (
   `email` varchar(150) DEFAULT NULL,
   `telefoon` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +95,7 @@ CREATE TABLE `gasten` (
 
 LOCK TABLES `gasten` WRITE;
 /*!40000 ALTER TABLE `gasten` DISABLE KEYS */;
-INSERT INTO `gasten` VALUES (1,'Mark','2025-12-06','IkbenMark@outlook.com','065587322'),(2,'Sophie','1998-03-12','sophie@mail.com','0612345678'),(3,'Liam','1985-07-22','liam@test.com','0623456789'),(4,'Noah','2001-11-04','noah@example.com','0634567890'),(5,'Emma','1993-09-30','emma@mail.net','0645678901'),(6,'Lucas','1980-01-15','lucas@domain.com','0656789012'),(7,'Julia','2005-05-08','julia@email.org','0667890123'),(8,'Finn','1997-12-19','finn@webmail.com','0678901234'),(9,'Mila','1989-02-27','mila@outlook.com','0689012345'),(10,'Daan','1995-08-14','daan@provider.nl','0690123456');
+INSERT INTO `gasten` VALUES (1,'Mark','2025-12-06','IkbenMark@outlook.com','065587322'),(2,'Sophie','1998-03-12','sophie@mail.com','0612345678'),(3,'Liam','1985-07-22','liam@test.com','0623456789'),(4,'Noah','2001-11-04','noah@example.com','0634567890'),(5,'Emma','1993-09-30','emma@mail.net','0645678901'),(6,'Lucas','1980-01-15','lucas@domain.com','0656789012'),(7,'Julia','2005-05-08','julia@email.org','0667890123'),(8,'Finn','1997-12-19','finn@webmail.com','0678901234'),(9,'Mila','1989-02-27','mila@outlook.com','0689012345'),(10,'Daan','1995-08-14','daan@provider.nl','0690123456'),(11,'Kaj Treep','1997-10-06','kajtreep@gmail.com','0646291419');
 /*!40000 ALTER TABLE `gasten` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -146,7 +177,7 @@ CREATE TABLE `reserveringen` (
   CONSTRAINT `fk_reservering_accommodatie` FOREIGN KEY (`accommodatie_type_id`) REFERENCES `accommodatie_types` (`id`),
   CONSTRAINT `reserveringen_ibfk_1` FOREIGN KEY (`staanplaats_id`) REFERENCES `staanplaatsen` (`id`),
   CONSTRAINT `reserveringen_ibfk_2` FOREIGN KEY (`reserveringhouder_id`) REFERENCES `gasten` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -155,7 +186,7 @@ CREATE TABLE `reserveringen` (
 
 LOCK TABLES `reserveringen` WRITE;
 /*!40000 ALTER TABLE `reserveringen` DISABLE KEYS */;
-INSERT INTO `reserveringen` VALUES (1,6,1,'2025-12-08','2025-12-09',0,0.00,1),(2,12,1,'2025-12-08','2025-12-09',0,0.00,1),(3,4,1,'2025-12-08','2025-12-09',0,0.00,1),(4,34,1,'2025-12-08','2025-12-09',0,0.00,2),(5,36,1,'2025-12-08','2025-12-09',0,0.00,2),(6,39,1,'2025-12-08','2025-12-15',0,0.00,4),(7,18,1,'2025-12-08','2025-12-15',0,0.00,1),(8,40,1,'2025-12-08','2025-12-15',0,0.00,4),(9,37,1,'2025-12-08','2025-12-15',0,0.00,3),(10,37,1,'2025-12-23','2025-12-30',0,0.00,3),(11,42,1,'2025-12-08','2025-12-29',0,0.00,2),(12,30,1,'2025-12-08','2025-12-29',0,0.00,2),(13,1,1,'2025-12-08','2025-12-29',0,0.00,1),(14,9,1,'2025-12-08','2025-12-22',0,0.00,1),(15,21,1,'2025-12-08','2025-12-09',0,0.00,2),(16,21,1,'2025-12-10','2025-12-13',0,0.00,3),(17,39,1,'2025-12-22','2025-12-29',0,0.00,4),(18,36,1,'2025-12-25','2025-12-31',0,0.00,2),(19,11,1,'2025-12-25','2025-12-31',0,0.00,1);
+INSERT INTO `reserveringen` VALUES (1,6,1,'2025-12-08','2025-12-09',0,0.00,1),(2,12,1,'2025-12-08','2025-12-09',0,0.00,1),(3,4,1,'2025-12-08','2025-12-09',0,0.00,1),(4,34,1,'2025-12-08','2025-12-09',0,0.00,2),(5,36,1,'2025-12-08','2025-12-09',0,0.00,2),(6,39,1,'2025-12-08','2025-12-15',0,0.00,4),(7,18,1,'2025-12-08','2025-12-15',0,0.00,1),(8,40,1,'2025-12-08','2025-12-15',0,0.00,4),(9,37,1,'2025-12-08','2025-12-15',0,0.00,3),(10,37,1,'2025-12-23','2025-12-30',0,0.00,3),(11,42,1,'2025-12-08','2025-12-29',0,0.00,2),(12,30,1,'2025-12-08','2025-12-29',0,0.00,2),(13,1,1,'2025-12-08','2025-12-29',0,0.00,1),(14,9,1,'2025-12-08','2025-12-22',0,0.00,1),(15,21,1,'2025-12-08','2025-12-09',0,0.00,2),(16,21,1,'2025-12-10','2025-12-13',0,0.00,3),(17,39,1,'2025-12-22','2025-12-29',0,0.00,4),(18,36,1,'2025-12-25','2025-12-31',0,0.00,2),(19,11,1,'2025-12-25','2025-12-31',0,0.00,1),(20,29,1,'2025-12-15','2025-12-22',0,0.00,1),(21,8,1,'2025-12-15','2025-12-22',0,0.00,1),(22,18,1,'2025-12-15','2025-12-22',0,0.00,1),(23,31,1,'2025-12-15','2025-12-31',0,0.00,2),(24,37,11,'2025-12-16','2025-12-18',0,0.00,3);
 /*!40000 ALTER TABLE `reserveringen` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -277,4 +308,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-14 23:56:15
+-- Dump completed on 2025-12-17 16:19:36
